1a668b62d906c05f2045e469c6e75959ef01a81d
tristandotdev <tristandepoosingh001@gmail.com>
2026-03-08 18:55:02 -0400

    fizzbuzz

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-let name = "bob";
-let age = 24;
-
-console.log(typeof(name))//string
-console.log(typeof(age))//number
-
-console.log(`Hello my name is ${name}, I'm ${age} years old`);
-//Hello my name is bob, I'm 24 years old.
-
-console.log(`I was born in ${2020 - age}`)
-//I was born in 1966
+for(let i = 0; i < 50; i++) {
+  if(i % 3 == 0 && i % 5 == 0) console.log("fizzbuzz");
+  else if(i % 3 == 0) console.log("fizz");
+  else if(i % 5 == 0) console.log("buzz");
+  else console.log(i);
+}
