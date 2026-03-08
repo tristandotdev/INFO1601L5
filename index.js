bc839cf2434acdbc2c5d36e81a08763691e91ead
tristandotdev <tristandepoosingh001@gmail.com>
2026-03-08 19:12:32 -0400

    BMI calc

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-let arr = [12, 33, 4, 5, -4, 8, 19, 25];
-                
-//map() creates a new array from the elements of one without changing the old one
-function double(num){
-  return num * 2;
+const people = [{
+    name: "James",
+    height: 60,
+    weight: 2.5
+  }, {
+    name: "Alice",
+    height: 81,
+    weight: 3
+  }, {
+    name: "Jason",
+    height: 50,
+    weight: 1.7
+  }
+];
+
+let sum = 0, count = 0;
+
+function calcBMI(weight, height) {
+  return weight / (height * height);
+};
+
+function printPerson(name, weight, height) {
+  console.log(`${name}'s weight: ${weight}\n${name}'s height: ${height}\n${name}'s BMI: ${calcBMI(weight, height)}\n`);
 }
 
-let doubledArr = arr.map(double); 
-console.log(doubledArr);
+people.forEach(element => {
+  printPerson(element.name, element.weight, element.height);
+  sum += calcBMI(element.weight, element.height);
+  count++;
+});
 
-function isOdd(num){
-  return num%2 !== 0; 
-}
-//Filter takes a test condition and returns only the element which 
-//make the condition true
-let odds = arr.filter(isOdd);
-console.log(odds);
-
-//Returns true or false if any of the elements of the array 
-//meets a specified condition
-function has5Factor(ele){
-  return ele % 5 === 0;
-}
-
-let hasFiveFactor = arr.some(has5Factor);
-console.log(hasFiveFactor);
-
-function intCompare(a, b){
- return a - b;
-}
-
-//sort function must return either 0, +ve, -ve
-let ascending = arr.sort(intCompare)
-console.log(ascending);
+console.log(`Average for ${count} persons was ${sum/count}`);
