e97a7bd357d220b56f6840256f9142feb4c613c7
tristandotdev <tristandepoosingh001@gmail.com>
2026-03-08 18:52:35 -0400

    strings

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let radius = 5;
-const pi = 3.14;
+let name = "bob";
+let age = 24;
 
-let area = radius * radius * pi;
-console.log(area);
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
