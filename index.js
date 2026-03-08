b5d0a2002c8c99decc360a76b73852cebc8ce26d
tristandotdev <tristandepoosingh001@gmail.com>
2026-03-08 18:59:45 -0400

    functions

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-let nowTimestamp = Date.now();//store the current Date
-
-console.log(nowTimestamp);//unix timestamp in milliseconds 1581291269848
-
-let now = new Date(nowTimestamp);//today's Date object 
-
-//months go from 0 - 11
-let date = new Date(2019, 11, 17, 3, 24, 0);//December 17, 2019 03:24:00
-//printing the date
-
-console.log(date.toLocaleDateString("en-US"));//12/17/2019
-//1 is added to month because January is 0
-console.log(` ${now.getDate()} - ${now.getMonth() + 1} - ${now.getFullYear()} `); // 9 - 2 - 2020 
-
-//Calculate difference between two dates in milliseconds
-let difference = now - date;
-//convert milliseconds to days
-
-let daysBetween = difference * 1.15741e-8;
-console.log(daysBetween);//54.6740790437368
-
-let root = Math.sqrt(9);// 3
-
-let cubed  = Math.pow(2, 3);// 8
-
-let roundDown = Math.floor(12.3453);// 12
-
-let roundUp = Math.ceil(12.3453);// 13
-
-let absolute = Math.abs(-34);// 34
-
-let randNum = Math.random();// random number between 0 and 1
-
-let pi = Math.pi;// pi in radians * 180 to convert to degrees
-
-//generate a random Integer between a supplied range
-function randInt(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function happyPrint(string){
+ console.log("😀: "+string);
 }
+
+function sadPrint(string){
+ console.log("😢: "+string);
+}
+
+//This high-order function adds 2 parameters and passes the answer to the callback
+function add(a, b, callback){
+   let ans = a + b;
+   callback(ans);// call the callback and pass the answer to it
+}
+
+//call add passing the callbacks to it
+
+add(5, 10, happyPrint);
+add(11, 12, sadPrint);
